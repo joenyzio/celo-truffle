f8c313c5fb3cb5a47ab3e42827523472a2b0f072
Joe Nyzio <jdnyzio@gmail.com>
2022-06-12 18:24:44 -0700

    change-owner

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
-var HelloCelo = artifacts.require("HelloCelo");
-module.exports = function (deployer) {
-  deployer.deploy(HelloCelo);
+var Owner = artifacts.require("Owner");
+
+module.exports = function(deployer) {
+    deployer.deploy(Owner);
+    // Additional contracts can be deployed here
 };
