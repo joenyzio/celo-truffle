fe42f3b9e11bc505648e5ce235774c72ad159810
Joe Nyzio <jdnyzio@gmail.com>
2022-06-12 18:26:47 -0700

    coin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,6 @@
-var HelloCelo = artifacts.require("HelloCelo");
-module.exports = function (deployer) {
-  deployer.deploy(HelloCelo);
+var Coin = artifacts.require("Coin");
+
+module.exports = function(deployer) {
+    deployer.deploy(Coin);
+    // Additional contracts can be deployed here
 };
